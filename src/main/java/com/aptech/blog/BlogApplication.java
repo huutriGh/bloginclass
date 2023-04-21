0443dae249cc9a3bab3a7d1707ef4f814da02b5a
nguyen huu tri <huutriqt13@hotmail.com>
2023-04-21 20:18:00 +0700

    modify basic authentication

--- src/main/java/com/aptech/blog/BlogApplication.java
+++ src/main/java/com/aptech/blog/BlogApplication.java
@@ -13,19 +13,19 @@ import com.aptech.blog.repository.BlogRepository;
 public class BlogApplication {
 
 	public static void main(String[] args) {
-	 var context =	SpringApplication.run(BlogApplication.class, args);
+		var context = SpringApplication.run(BlogApplication.class, args);
 
-		// BlogRepository repository = context.getBean(BlogRepository.class);
-		// Random random = new Random();
+		BlogRepository repository = context.getBean(BlogRepository.class);
+		Random random = new Random();
 
-		// for (int i = 1; i <= 1000; i++) {
-		// int rating = random.ints(1,5).findFirst().getAsInt();
-		// Blog blog = new Blog();
-		// blog.setBlogId(i).setTitle("Title " + i)
-		// .setUrl("http://localhost:8080/blog/" + i)
-		// .setRating(rating);
-		// repository.save(blog);
-		// }
+		for (int i = 1; i <= 1000; i++) {
+			int rating = random.ints(1, 5).findFirst().getAsInt();
+			Blog blog = new Blog();
+			blog.setBlogId(i).setTitle("Title " + i)
+					.setUrl("http://localhost:8080/blog/" + i)
+					.setRating(rating);
+			repository.save(blog);
+		}
 
 	}
 
